318d81a14b021dc534cc8460d3bb46a5d848ae5d
nacim <nacim@TECH-06.asylum.local>
2024-01-30 11:52:47 +0100

    Création des tokens, et possibilité de se connecter avec droits pour faire requêtes

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -6,14 +6,15 @@ use App\Entity\Player;
 use App\Repository\PlayerRepository;
 use App\Repository\TeamRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PlayerController extends AbstractController
@@ -43,6 +44,7 @@ class PlayerController extends AbstractController
     }
 
     #[Route('/api/players', name:"createPlayer", methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN', message:"Vous devez disposer des droits pour pouvoir créer un joueur")]
     public function createPlayer(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator, TeamRepository $teamRepository, ValidatorInterface $validator): JsonResponse 
     {
         $player = $serializer->deserialize($request->getContent(), Player::class, 'json');
